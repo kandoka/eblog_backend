ba74332947570e98079ef354e576746348349e6a
kandoka <kandoka@126.com>
2020-11-25 15:13:25 +0800

    添加前端模板

---
+++ src/main/java/com/kandoka/EblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kandoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EblogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EblogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kandoka/EblogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kandoka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EblogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
